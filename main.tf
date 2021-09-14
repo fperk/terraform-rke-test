cb58c638148de4cd940c13870b5b337f95a67421
fperk <dragonl33t@gmail.com>
2021-09-14 15:11:26 +0200

    Fix module reference

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "k8s_cluster" {
-  source = "https://github.com/brotandgames/terraform-hcloud-rke.git?ref=master"
+  source = "git::https://github.com/brotandgames/terraform-hcloud-rke.git?ref=master"
 
   addons_include = [
     "https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml",
